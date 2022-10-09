12c1ccca764fad9c94419fe419f368f9b3295b9b
Victor Ribeiro Gammaro <victorgammaro@gmail.com>
2022-10-09 20:03:42 -0300

    Home

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+import HomePage from './src/Pages/Homepage'
+
+const App = () => {
+
+  return (
+
+    <HomePage />
+
+  );
+
+}
+
+export default App;
